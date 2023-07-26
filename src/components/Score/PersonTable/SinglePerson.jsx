fc1d9a08ace3b4c378bb515019a353104e5a84be
RamFarid <workprojects22@gmail.com>
2023-07-26 22:17:01 +0300

    Add qrcode image

--- src/components/Score/PersonTable/SinglePerson.jsx
+++ src/components/Score/PersonTable/SinglePerson.jsx
@@ -1,15 +1,34 @@
-import { TableCell, TableRow, styled } from '@mui/material'
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Stack,
+  TableCell,
+  TableRow,
+  Typography,
+  styled,
+} from '@mui/material'
 import ActionsCell from '../ActionsCell'
-import { useCallback, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import { toast } from 'react-toastify'
 import { useScore } from '../../../Contexts/ScoreContext'
 import { SERVER_URL, useUser } from '../../../Contexts/UserContext'
 import SuperscriptIcon from '@mui/icons-material/Superscript'
+import generateQRImg from '../../../utils/generateQRImg'
+import html2canvas from 'html2canvas'
 
 function SinglePerson({ person, onEdit, i }) {
   const [isLoading, setIsLoading] = useState(false)
   const { online, isLoggedIn } = useUser()
   const { removePerson, updatePerson } = useScore()
+  const qrCodeImageRef = useRef(null)
+  const [qrCode, setQrCode] = useState({
+    isQRCode: false,
+    qrCodeImage: '',
+    qrCodeDowmload: '',
+  })
   const deleteHandler = useCallback(async () => {
     if (!online) return toast('طب ما انت معكش نت')
     try {
@@ -73,7 +92,37 @@ function SinglePerson({ person, onEdit, i }) {
         }}
       >
         <CustomCell align='right'>{i || '107'}</CustomCell>
-        <CustomCell align='right'>{person.name}</CustomCell>
+        <CustomCell
+          align='right'
+          onClick={async () => {
+            try {
+              setIsLoading(true)
+              const link = await generateQRImg(person._id)
+              setQrCode((pre) => ({
+                ...pre,
+                isQRCode: true,
+                qrCodeImage: link,
+              }))
+              setTimeout(() => {
+                const divToCapture = qrCodeImageRef.current
+                html2canvas(divToCapture)
+                  .then((canvas) => {
+                    const screenshotUrl = canvas.toDataURL('image/png')
+                    setQrCode((pre) => ({
+                      ...pre,
+                      qrCodeDowmload: screenshotUrl,
+                    }))
+                  })
+                  .finally(() => setIsLoading(false))
+              }, 1000)
+              // Use html2canvas to take the screenshot
+            } catch (error) {
+              console.log(error)
+            }
+          }}
+        >
+          {person.name}
+        </CustomCell>
         <CustomCell align='right'>
           {person.grade === 1 ? 'اولى' : person.grade === 2 ? 'تانيه' : 'تالته'}
         </CustomCell>
@@ -94,6 +143,39 @@ function SinglePerson({ person, onEdit, i }) {
           />
         )}
       </TableRow>
+      <Dialog
+        open={qrCode.isQRCode}
+        onClose={() => setQrCode({ isQRCode: false })}
+      >
+        <DialogTitle>QR code {person.name}</DialogTitle>
+        <DialogContent>
+          <Stack ref={qrCodeImageRef}>
+            <Box
+              component={'img'}
+              src={qrCode.qrCodeImage}
+              alt='Photo of user'
+              maxWidth={'300px'}
+              maxHeight={'300px'}
+              overflow={'hidden'}
+              mx={'auto'}
+            />
+            <Typography align='center' variant='h5'>
+              {person.name}
+            </Typography>
+          </Stack>
+          <Button
+            fullWidth
+            disabled={isLoading}
+            sx={{ mt: 3 }}
+            component={'a'}
+            href={qrCode.qrCodeDowmload}
+            variant='contained'
+            download={`${person.name}-qrcode.png`}
+          >
+            {isLoading ? 'بحمل الqrcode...' : 'نزل الصورة'}
+          </Button>
+        </DialogContent>
+      </Dialog>
     </>
   )
 }
